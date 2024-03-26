93bbd1f8e6172d35a0f59988581112d147b02420
Jakub Chmielewski <j.j.chmielewski@wp.pl>
2024-03-26 18:19:52 +0100

    Added expenses

---
+++ IZO/Models/Expenses/Expense.cs
@@ -0,0 +1,8 @@
+﻿namespace IZO.Models.Expenses
+{
+    public class Expense
+    {
+        public double moneySpent { get; set; } = 0;
+        public ExpenseCategory category { get; set; } = ExpenseCategory.MISC;
+    }
+}
---
+++ IZO/Models/Expenses/MonthlyExpenses.cs
@@ -0,0 +1,8 @@
+﻿namespace IZO.Models.Expenses
+{
+    public class MonthlyExpenses
+    {
+        public Dictionary<ExpenseCategory, Expense> fixedExpenses { get; set; } = new Dictionary<ExpenseCategory, Expense>();
+        public Dictionary<ExpenseCategory, Expense> dayToDayExpenses { get; set; } = new Dictionary<ExpenseCategory, Expense>();
+    }
+}
