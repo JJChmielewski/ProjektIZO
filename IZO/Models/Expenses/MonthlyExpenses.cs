ce051552368b0bb74449d30b0e56e2182910dc82
KacZeg <k.zeglen@gazeta.pl>
2024-04-07 13:34:53 +0200

    Added UI with expenses tables and layout for other features

--- IZO/Models/Expenses/MonthlyExpenses.cs
+++ IZO/Models/Expenses/MonthlyExpenses.cs
@@ -12,13 +12,15 @@
                 fixedExpenses = new Dictionary<ExpenseCategory, Expense>
                 {
                     { ExpenseCategory.HOUSING, new Expense(1200, ExpenseCategory.HOUSING, DateTime.Now) },
-                    
+                    { ExpenseCategory.UTILITIES, new Expense(200, ExpenseCategory.UTILITIES, DateTime.Now) }
+
                 },
                 dayToDayExpenses = new Dictionary<ExpenseCategory, Expense>
                 {
                     { ExpenseCategory.ENTERTAINMENT, new Expense(300, ExpenseCategory.ENTERTAINMENT, DateTime.Now) },
                     { ExpenseCategory.GROCERIES, new Expense(150, ExpenseCategory.GROCERIES, DateTime.Now) },
-                    { ExpenseCategory.TRANSIT, new Expense(50, ExpenseCategory.TRAVEL, DateTime.Now) }
+                    { ExpenseCategory.TRANSIT, new Expense(50, ExpenseCategory.TRAVEL, DateTime.Now) },
+                    { ExpenseCategory.CLOTHES, new Expense(500, ExpenseCategory.CLOTHES, DateTime.Now) }
                 }
             };
 
