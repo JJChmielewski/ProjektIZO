22a4d6380761639d05cd3fbafdb1abbdc9befaa5
Dell-Tomasz <richert.tomasz@gmail.com>
2024-05-11 13:59:50 +0200

    Added changes related to Saving calculator Added - Earning Controller, Models: EarningPieChart, Earning, EarningCategory, MonthlyEarnings, Service : EarningAccesorService

---
+++ IZO/Models/Charts/EarningsPieChart.cs
@@ -0,0 +1,38 @@
+﻿using IZO.Models.Earnings;
+using IZO.Models.Expenses;
+using IZO.Services;
+
+namespace IZO.Models.Charts
+{
+    public class EarningsPieChart
+    {
+        public Dictionary<EarningCategory, decimal> EarningsByCategory { get; set; } = new Dictionary<EarningCategory, decimal>();
+
+        public static EarningsPieChart initChartData() // ------ HERE IS MOCK DATA FOR PIE CHART ------ //
+        {
+
+           
+
+            var earningsByCategoryFixed = EarningAccesorService.monthlyEarnings.fixed1Earnings
+                .GroupBy(earning => earning.Key)
+                .ToDictionary(group => group.Key, group => group.Sum(earnings => earnings.Value.Sum(e => (decimal)e.moneyEarn)));
+
+            var earningsByCategory = EarningAccesorService.monthlyEarnings.dayToDayEarnings
+                .GroupBy(earning => earning.Key)
+                .ToDictionary(group => group.Key, group => group.Sum(earnings => earnings.Value.Sum(e => (decimal)e.moneyEarn)));
+
+            foreach (var row in earningsByCategoryFixed)
+            {
+                if (earningsByCategory.ContainsKey(row.Key))
+                {
+                    earningsByCategory[row.Key] = row.Value + earningsByCategory[row.Key];
+                    continue;
+                }
+
+                earningsByCategory.Add(row.Key, row.Value);
+            }
+
+            return new EarningsPieChart { EarningsByCategory = earningsByCategory };
+        }
+    }
+}
